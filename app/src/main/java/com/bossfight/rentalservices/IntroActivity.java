e4fc15788ff19f6576f6b9fe3596671252476030
Sumakhursheed <suma_khursheed@hotmail.com>
2017-11-26 13:50:47 -0800

    IntroActivity changes

--- app/src/main/java/com/bossfight/rentalservices/IntroActivity.java
+++ app/src/main/java/com/bossfight/rentalservices/IntroActivity.java
@@ -2,6 +2,7 @@ package com.bossfight.rentalservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Log.e("msg","msg");
+
     }
 }
