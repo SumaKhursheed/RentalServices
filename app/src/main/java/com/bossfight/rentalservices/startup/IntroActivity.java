d32a7a774867f288f80e98a8cfbb44cde0cd2457
Sumakhursheed <suma_khursheed@hotmail.com>
2017-11-27 23:23:00 -0800

    Registration and SignIn added

--- app/src/main/java/com/bossfight/rentalservices/startup/IntroActivity.java
+++ app/src/main/java/com/bossfight/rentalservices/startup/IntroActivity.java
@@ -1,8 +1,10 @@
-package com.bossfight.rentalservices;
+package com.bossfight.rentalservices.startup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+
+import com.bossfight.rentalservices.R;
 
 public class IntroActivity extends AppCompatActivity {
 
